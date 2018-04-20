2b52247310bdade4018e2281098558d0c1470ef2
quellodellecomunicazioni <38555389+quellodellecomunicazioni@users.noreply.github.com>
2018-04-20 05:38:16 -0700

    Update and rename spanningTree.py to topology.py
    Added few lines to draw a graph of the simulated network and comments with the instructions to run the program

--- topology.py
+++ topology.py
@@ -1,3 +1,7 @@
+# to run this use "ryu-manager --observe-links topology.py" 
+# the programs works fine with simple topologies but still gives multiple outputs when working with tree topologies
+# be sure you installed networkx and matplotlib
+
 from ryu.base import app_manager
 from ryu.controller import ofp_event
 from ryu.controller.handler import MAIN_DISPATCHER
@@ -7,9 +11,10 @@ from ryu.lib.mac import haddr_to_bin
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
 from ryu.lib.packet import ether_types
-
 from ryu.topology import event, switches
 from ryu.topology.api import get_switch, get_link
+import networkx as nx			# necessary to draw the graph of the network
+import matplotlib.pyplot as plt		# necessary to show the drawing of the graph obtained with nx.draw()
 
 class SimpleSwitch(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_0.OFP_VERSION]
@@ -98,4 +103,9 @@ class SimpleSwitch(app_manager.RyuApp):
 	print "switches: ", switches
         links_list = get_link(self, None)
         links=[(link.src.dpid,link.dst.dpid,{'port':link.src.port_no}) for link in links_list]
-	print "links", links
+	print "links:", links
+	G = nx.Graph()			
+	G.add_nodes_from(switches)
+	G.add_edges_from(links)
+	nx.draw(G)
+	plt.show()
