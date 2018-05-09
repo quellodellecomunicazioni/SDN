bd703994cee9be45dd3be2cbf98fd7ff273b93eb
Davide <38555389+quellodellecomunicazioni@users.noreply.github.com>
2018-05-09 20:44:39 +0200

    Update discovery.py

--- discovery.py
+++ discovery.py
@@ -27,10 +27,15 @@ def check_links(root):
     return marius
 
 def nodi_collegati(node):
+    '''
+    nodi_collegati mi restituisce una lista di nodi collegati al nodo in ingresso
+    '''
     neighbors = []
     for i in range(0, len(edges)):
         if edges[i][0] == node:
             neighbors.append(edges[i][1])
+        elif edges[i][1] == node:
+            neighbors.append(edges[i][0])
     return neighbors
 
 def shortest_path(path):
@@ -45,7 +50,7 @@ def shortest_path(path):
     else:                       # vuol dire che ho un collegamento indiretto
         #jumps = 0
         # guardo tutti i links con dst
-        for i in edges:
+        for i in range(0,len(edges)):
             if edges[i][1] == dst:
                 if edges[i][0] in nodi_collegati(src):
                     next_hop = edges[i][0]
@@ -55,8 +60,9 @@ def add_rule(path):
     '''
     add_rule aggiunge le regola che soddisfa il percorso (src, dst)
     '''
-    next_hop = shortest_path(path)
-    rules[path] = next_hop
+    if path not in rules:
+        next_hop = shortest_path(path)
+        rules[path] = next_hop
     return
 
 def clearList(dirty):
@@ -73,8 +79,8 @@ for root in raw_nodes:
     edges = edges + check_links(root)
     edges = clearList(edges)
     nodes.append(root)
-    for j in range(0, len(edges)): # aggiunge solo le regole progressive
-        add_rule(edges[j])
-        print(rules)
+    for j in range(0, len(edges)):      # aggiunge solo le regole progressive
+        if edges[j] not in rules:
+            add_rule(edges[j])
     #progressive_graph(nodes, edges)
-#print(rules)
+print(rules)
