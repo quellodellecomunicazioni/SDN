656a7790ce29cdfa1002f53c78fa6b011f57470d
Davide <38555389+quellodellecomunicazioni@users.noreply.github.com>
2018-05-09 19:53:46 +0200

    Add files via upload

---
+++ discovery.py
@@ -0,0 +1,80 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+raw_nodes = [1, 2, 3, 4, 5]
+raw_edges = [(1,2), (1,3), (1,4), (3,5), (4,5)]
+
+rules = {}
+
+def progressive_graph(nodes, links):
+    '''
+    progressive_graph disegna il grafo della rete in maniera progressiva, aggiungendo di volta in volta i nodi e i collegamenti trovati
+    '''
+    G = nx.Graph()
+    G.add_nodes_from(nodes)
+    G.add_edges_from(links)
+    nx.draw(G)
+    plt.show()
+
+def check_links(root):
+    '''
+    check_links restituisce una lista contenente tutti i collegamenti uscenti/entranti da root
+    '''
+    marius = []
+    for i in range(0,len(raw_edges)):
+        if raw_edges[i][0] == root or raw_edges[i][1] == root:
+            marius.append(raw_edges[i])
+    return marius
+
+def nodi_collegati(node):
+    neighbors = []
+    for i in range(0, len(edges)):
+        if edges[i][0] == node:
+            neighbors.append(edges[i][1])
+    return neighbors
+
+def shortest_path(path):
+    '''
+    calcola il percorso più breve tra src e dst
+    '''
+    src = path[0]
+    dst = path[1]
+    if path in edges:           # vuol dire che esiste il collegamento diretto
+        next_hop = dst
+        return next_hop
+    else:                       # vuol dire che ho un collegamento indiretto
+        #jumps = 0
+        # guardo tutti i links con dst
+        for i in edges:
+            if edges[i][1] == dst:
+                if edges[i][0] in nodi_collegati(src):
+                    next_hop = edges[i][0]
+                    return next_hop
+
+def add_rule(path):
+    '''
+    add_rule aggiunge le regola che soddisfa il percorso (src, dst)
+    '''
+    next_hop = shortest_path(path)
+    rules[path] = next_hop
+    return
+
+def clearList(dirty):
+    '''
+    clearList elimina gli elementi ripetuti
+    '''
+    clear = list(set(dirty))
+    return clear
+
+G = nx.Graph()
+edges = []
+nodes = []
+for root in raw_nodes:
+    edges = edges + check_links(root)
+    edges = clearList(edges)
+    nodes.append(root)
+    for j in range(0, len(edges)): # aggiunge solo le regole progressive
+        add_rule(edges[j])
+        print(rules)
+    #progressive_graph(nodes, edges)
+#print(rules)
