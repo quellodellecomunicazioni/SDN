400778ea197522311b0867079c7aa2f4ffdb6cfb
Davide <38555389+quellodellecomunicazioni@users.noreply.github.com>
2018-05-14 00:44:57 +0200

    Update discovery.py
    Now mirror links are added simultaneously to the direct links.
    Progressive_graph was eliminated
    A new way to calculate the shortest path was added, it still needs to be modified so that in case one node has 2+ ways to reach its dst, the one with the lowest weight is chosen

--- discovery.py
+++ discovery.py
@@ -6,19 +6,6 @@ raw_edges = [(1,2), (1,3), (1,4), (3,5), (4,5)]
 
 rules = {}
 
-def progressive_graph(nodes, links):
-    '''
-    progressive_graph disegna il grafo della rete in maniera progressiva,
-    aggiungendo di volta in volta i nodi e i collegamenti trovati
-    '''
-    G = nx.Graph()
-    G.add_nodes_from(nodes)
-    G.add_edges_from(links, weight=1)   # weight = 1 assegna un peso al link
-    # assegno weight = 1 perché i link scoperti in maniera progressiva sono link diretti
-    # per accedere all`attributo di un link del tipo (src,dst) devo fare G[src][dst]['weight']
-    nx.draw(G)
-    plt.show()
-
 def check_links(root):
     '''
     check_links restituisce una lista contenente tutti i collegamenti uscenti/entranti da root
@@ -45,23 +32,24 @@ def nodi_collegati(node):
 def shortest_path(path):
     '''
     calcola il percorso più breve tra src e dst
+    esiste una regola da uno dei vicini alla dst? Se sì, scelgo quella con il peso minore
     '''
     src = path[0]
     dst = path[1]
-    if len(nodi_collegati(src)) == 1:
+    if len(nodi_collegati(src)) == 1:       # con un solo collegamento non ho opzioni
         next_hop = nodi_collegati(src)[0]
         return next_hop
     if path in edges:           # vuol dire che esiste il collegamento diretto
         next_hop = dst
         return next_hop
     else:                       # vuol dire che ho un collegamento indiretto
-        #jumps = 0
-        # guardo tutti i links con dst
-        for i in range(0,len(edges)):
-            if edges[i][1] == dst:
-                if edges[i][0] in nodi_collegati(src):
-                    next_hop = edges[i][0]
-                    return next_hop
+        n = []
+        n = nodi_collegati(src)
+        for i in range(0, len(n)):
+            n_path = (n[i], dst)
+            if n_path in rules:
+                next_hop = n[i]
+                return next_hop
 
 def add_rule(path):
     '''
@@ -89,16 +77,19 @@ for root in raw_nodes:
     for j in range(0, len(edges)):      # aggiunge solo le regole progressive
         if edges[j] not in rules:
             add_rule(edges[j])
-    progressive_graph(nodes, edges)
+            t = edges[j][0]
+            r = edges[j][1]
+            mirror = (r,t)
+            edges.append(mirror)
+            add_rule(mirror)
+    #progressive_graph(nodes, edges)
+    G.add_nodes_from(nodes)
+    G.add_edges_from(edges, weight=1)   # weight = 1 assegna un peso al link
+    # assegno weight = 1 perché i link scoperti in maniera progressiva sono link diretti
+    # per accedere all`attributo di un link del tipo (src,dst) devo fare G[src][dst]['weight']
+    nx.draw(G)
+    plt.show()
 #print(rules)
-
-# aggiunge i cammini diretti in direzione opposta e ne carica le regole
-for i in range(0,len(edges)):
-    t = edges[i][0]
-    r = edges[i][1]
-    s = (r,t)
-    edges.append(s)
-    add_rule(s)
 
 indirect_links = []
 for i in range(0,len(nodes)):
